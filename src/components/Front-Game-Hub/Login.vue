5358854e4817e2e359ef3d46d78572d13ab58d1d
Natasia09 <68784670+Natasia09@users.noreply.github.com>
2022-04-17 01:20:20 +0700

    Update Login.vue
    add css

--- src/components/Front-Game-Hub/Login.vue
+++ src/components/Front-Game-Hub/Login.vue
@@ -72,7 +72,10 @@ const login = () => {
  
 <style scoped>
 .body {
-  background-color: #f1f1f1;
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  color: white;
+  /* background-color: #f1f1f1; */
   padding-bottom: 40vh;
   font-family: 'Poppins', sans-serif;
 }
