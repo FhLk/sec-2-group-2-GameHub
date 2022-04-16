2b0cbeba97a046cdfa7e954df28c2f2b93a2724c
Natasia09 <68784670+Natasia09@users.noreply.github.com>
2022-04-17 01:22:00 +0700

    Update Register.vue
    add css

--- src/components/Front-Game-Hub/Register.vue
+++ src/components/Front-Game-Hub/Register.vue
@@ -74,7 +74,10 @@ checkIsOnline();
  
 <style scoped>
 .body {
-  background-color: #f1f1f1;
+  /* background-color: #f1f1f1; */
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  color: white;
   padding-bottom: 25vh;
   font-family: 'Poppins', sans-serif;
 }
